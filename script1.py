7452b687ba883f703a2d2eb0923766458208db34
rachel868 <rachel_bailey5@baylor.edu>
2023-08-29 16:41:43 -0500

    initial commit

--- script1.py
+++ script1.py
@@ -1,19 +1,11 @@
 #### PROBLEM 1 ####
-# Correc the mistakes and produce the following output:
+# Correct the mistakes and produce the following output:
 # Johnny, the total of your purchase is $1,688.70
-
-def calcTotal(amt,tax,name):
-    mytotal = amt * (1+tax)
-    print(f"{myname}, the total of your purchase is ${total:,.2f}")
-
 myname = 'Johnny'
-price = 1,560
-thetax = 8.25%
-
-calcTotal(thetax,price,name)
-
-
-
+amt = 1560
+tax = .0825
+mytotal = amt * (1+tax)
+print(f"{myname}, the total of your purchase is ${mytotal:,.2f}")
 
 
 ####  PROBLEM 2  ####
@@ -21,9 +13,8 @@ calcTotal(thetax,price,name)
 list1 = [1,'two',3.2,'four',5]
 
 for i in list1:
-    print(i)
-
-
+    if type(i) == int:
+        print(i)
 
 
 #### PROBLEM 3 ####
